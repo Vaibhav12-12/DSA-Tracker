2aaf48973f640b426d4709a617e5dc84ad85bcf6
Vaibhav12-12 <b.vaibhav.0012@gmail.com>
2024-12-20 16:08:09 +0530

    Removed boilerplate code, created components and started using router

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <div className="flex justify-center">
+      <div className="text-center mb-6 bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-3xl font-black">
+        DSA Tracker
+      </div>
+    </div>
+  );
+}
